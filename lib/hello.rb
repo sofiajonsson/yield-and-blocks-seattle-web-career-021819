b1d3b38c1bd0096c596bc994317be335ce7d8513
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-06 21:07:59 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,11 @@
 def hello_t(array)
-#   i = 0 
+  i = 0 
   
-#   while i < array.length 
-#     yield array[i]
-#     i += 1 
-#   end 
-# end
+  while i < array.length 
+    yield array[i]
+    i += 1 
+  end 
+end
 
 # call your method here!
 
